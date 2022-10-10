bbeceff11c0413bf915e8b8a19e7a9c4fe34d52e
Eva González <evagonzalez.tic@gmail.com>
2022-10-10 18:33:22 +0200

    exercise3

--- src/components/Ships/Ships.js
+++ src/components/Ships/Ships.js
@@ -1,32 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { GlobalStyle, Container } from "./Ships.styles";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Ships = () => {
   const [starShips, setStarShips] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
-    fetch("https://swapi.dev/api/starships/")
+    fetch("https://swapi.dev/api/starships/?page=" + page)
       .then((res) => res.json())
-      .then((res) => setStarShips(res.results));
-  }, []);
-
+      .then((res) => {
+        setStarShips((prevShips) => prevShips.concat(res.results));
+        setHasMore((res.next === null? false : true));
+      });
+  }, [page]);
 
   return (
-    <>
-    <GlobalStyle/>
-    
+    <InfiniteScroll
+      dataLength={starShips.length}
+      hasMore={hasMore}
+      next={() => setPage((prevPage) => prevPage + 1)}
+    >
+      <GlobalStyle />
+
       {starShips.map((ship) => (
         <Container key={ship.name}>
-          
-          <Link style={{color:'grey', textDecoration:'none'}} to={`/shipdetails/${Number(ship.url.split("/").slice(-2)[0])}`}>
+          <Link
+            style={{ color: "grey", textDecoration: "none" }}
+            to={`/shipdetails/${Number(ship.url.split("/").slice(-2)[0])}`}
+          >
             {ship.name.toUpperCase()}
           </Link>
           <p>{ship.model}</p>
         </Container>
       ))}
-     
-    </>
+    </InfiniteScroll>
   );
 };
 
